b6014d2544aad3a66a63d02e1ebdb2dbe3f9e741
Richard Beverlo <richard.beverlo@fritom.nl>
2022-09-07 16:06:14 +0200

    Added todo functionality

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useTodoListStore } from '@/stores/todoList';
+
+const todo = ref('')
+const input = ref<HTMLInputElement>()
+
+const store = useTodoListStore()
+
+const addItemAndClear = (item: string) => {
+  store.addTodo(item)
+  todo.value = ''
+  input.value?.focus()
+}
+
+
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="addItemAndClear(todo)">
+      <input v-model="todo" type="text" ref="input" />
+      <button>Add</button>
+    </form>
+  </div>
+</template>
